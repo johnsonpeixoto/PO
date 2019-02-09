48273ec432564b4e40a8eeb8fea2c74cc1115c02
johnsonpeixoto <johnson@fotonica.ifce.edu.br>
2019-02-08 22:58:03 -0300

    Atividade para comparar casos com mesmo tamanho de entrada

--- bubble_v2/main.py
+++ bubble_v2/main.py
@@ -1,5 +1,5 @@
 from random import randint
-import time 
+import timeit
 
 import matplotlib as mpl
 mpl.use('Agg')
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt
 
 def geraLista(tam, caso): #1- melhor /2- medio /3- pior
 	lista = []
-	if (caso == 1):
+	if (caso == "melhor"):
 		for i in range(0, tam):
 			lista.append(i)
-	elif (caso == 2):
+	elif (caso == "medio"):
 		for i in range(0, tam):
 			n = randint(1, 1 * tam)
 			lista.append(n)
-	elif (caso == 3):
+	elif (caso == "pior"):
 		for i in range(tam-1, -1, -1):
 			lista.append(i)
 	else:
@@ -23,36 +23,34 @@ def geraLista(tam, caso): #1- melhor /2- medio /3- pior
 	return lista
 
 def bubbleSort(serie): # sem numero de swaps
-    for num in range(len(serie) - 1, 0, -1):
+    for num in range(len(serie) - 1, -1, -1):
         for i in range(num):
-            if serie[i] > serie[i + 1]:
+            if serie[i] > serie[i+1]:
                 temp = serie[i]
                 serie[i] = serie[i + 1]
                 serie[i + 1] = temp
 
-def geraGraf(x,y,xl,yl,lb,filew):
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111)
-    ax.plot(x, y, label = lb)
-    ax.legend(bbox_to_anchor=(1, 1),			bbox_transform=plt.gcf().transFigure)
-    plt.ylabel(yl)
-    plt.xlabel(xl)
-    fig.savefig(filew)
+def geraGraf(x,y,xl,yl,lab, filew):
+	fig = plt.figure(figsize=(10, 10))
+	ax = fig.add_subplot(111)
+	ax.plot(x, y, label= lab, marker='o', markerfacecolor='blue', markersize=12)
+	ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
+	plt.ylabel(yl)
+	plt.xlabel(xl)
+	fig.savefig(filew)
 
 series = [1000, 2000, 3000, 4000, 6000]
-casos = [1,2,3]
-swaps = []
+casos = ["melhor","medio","pior"]
+#swaps = []
 tempos = []
 
 for tam in series:
-    tempos = []
-    for c in casos:
-        lista = geraLista(tam,c)
-        inicio = time.time()
-        bubbleSort(lista)
-        tempo = time.time() - inicio
-        #tempo = timeit.timeit("bubbleSort({})".format(lista),setup="from __main__ import bubbleSort",number=1)
-        tempos.append(tempo)
-    geraGraf(casos, tempos, "series", "tempo", "tempo de processo", str(tam)+"_"+str(casos)+".png")
+	print(tempos)
+	tempos = []
+	for c in casos:
+        	lista = geraLista(tam, c)
+        	#bubbleSort(lista)
+        	tempos.append(timeit.timeit("bubbleSort({})".format(lista),setup="from __main__ import bubbleSort", number=1))
+	geraGraf(casos, tempos, "Casos", "Tempo", "Tempo da Serie "+str(tam), "casos de "+str(tam)+".png")
 
 print("Finish")
