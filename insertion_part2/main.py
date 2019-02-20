6a1d0c2b085d069f1d00b03e0b7b1d1c2ceab90e
johnsonpeixoto <johnson@fotonica.ifce.edu.br>
2019-02-20 19:36:30 -0300

    update

--- insertion_part2/main.py
+++ insertion_part2/main.py
@@ -5,13 +5,16 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-def selectionSort(serie): # arg.: lista com as series a seres ordenadas
-	for i in range(len(serie)-1, 0, -1):
-		menor = 0
-		for j in range(1, i+1):
-			if(serie[j] > serie[menor]):
-				menor = j
-		serie[i], serie[menor] = serie[menor], serie[i] 
+def insertionSort(serie):
+	for i in range(1,len(serie)):
+		valor = serie[i]
+		indice = i
+
+		while indice>0 and serie[indice-1]>valor:
+			serie[indice]=serie[indice-1]
+			indice = indice-1
+
+		serie[indice]=valor
 	return serie
 
 def geraLista(tam): # lista de permutações com filtro de tupla
@@ -28,14 +31,16 @@ def tempListas(serie): # executa selection em cada caso
 	tempo = []
 	for i in range(0, len(serie)):
 		print(serie[i])
-		tempo.append(timeit.timeit("selectionSort({})".format(serie[i]),setup="from __main__ import selectionSort", number=1))
+		tempo.append(timeit.timeit("insertionSort({})".format(serie[i]),setup="from __main__ import insertionSort", number=1))
 	return tempo
 
 #######################################################
-########	INICIO DO PROGRAMA	###############
+###########   INICIO DO PROGRAMA   ###################
 #######################################################
 
 series = geraLista(6) # gera uma lista de series de tamanho 6 com todas as permutaçõess
 tempos = tempListas(series) # crio uma lista com tempo de ordenamento de cada permutação
-print ("O pior caso é a sequência: "+str(series[tempos.index(max(tempos))]))
-print ("O melhor caso é a sequência: "+str(series[tempos.index(min(tempos))]))
+print (tempos)
+print ("O pior caso foi a sequência: "+str(series[tempos.index(max(tempos))]))
+print ("O melhor caso foi a sequência: "+str(series[tempos.index(min(tempos))]))
+
