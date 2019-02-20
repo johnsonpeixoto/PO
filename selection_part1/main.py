eed7dbb2b199f35bc60e6e747480adbc68e5c4bc
johnsonpeixoto <johnson@fotonica.ifce.edu.br>
2019-02-20 19:35:51 -0300

    update

--- selection_part1/main.py
+++ selection_part1/main.py
@@ -20,36 +20,34 @@ def geraLista(tam, caso): #1- melhor /2- medio /3- pior
 	return lista
 
 def selectionSort(serie): # arg.: lista com as series a seres ordenadas
-	print(serie)
 	for i in range(len(serie)-1, 0, -1):
 		menor = 0
 		for j in range(1, i+1):
 			if(serie[j] > serie[menor]):
 				menor = j
 		serie[i], serie[menor] = serie[menor], serie[i]
-	print(serie)
 	return serie
 
 
-def geraGraf(x,y,xl,yl,lab, filew):
+def geraGraf(x, lab, y, xl, yl, filew):
+	cores = ['blue', 'green', 'red', 'purple', 'black']
 	fig = plt.figure(figsize=(10, 10))
 	ax = fig.add_subplot(111)
-	ax.plot(x, y, label= lab, marker='o', markerfacecolor='blue', markersize=12)
+	for i in range(0, len(lab)):
+		ax.plot(x, y[i], label=lab[i], markerfacecolor=cores[i], markersize=12)
 	ax.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
 	plt.ylabel(yl)
 	plt.xlabel(xl)
 	fig.savefig(filew)
-series = [10, 20, 30]
 
-#series = [1000, 2000, 3000, 4000, 6000]
+series = [1000, 2000, 3000, 4000, 6000]
 casos = ["melhor","medio","pior"]
 tempos = []
 for tam in series:
-	tempos = []
+	t = []
 	for c in casos:
-		lista = geraLista(tam, c)
-		tempos.append(timeit.timeit("selectionSort({})".format(lista),setup="from __main__ import selectionSort", number=1))
-	print(tempos)
-	geraGraf(casos, tempos, "Casos", "Tempo", "Tempo da Serie "+str(tam), "casos de "+str(tam)+".png")
+		t.append(timeit.timeit("selectionSort({})".format(geraLista(tam, c)),setup="from __main__ import selectionSort", number=1))
+	tempos.append(t)
+geraGraf(casos, series, tempos, "Casos", "Tempo", "tempos.png")
 
 print("Finish")
